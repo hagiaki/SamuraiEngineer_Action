c9e54a02a6b97a19dc734667352368cf6b069503
Hagiaki <hagiaki.favorite@gmail.com>
2022-02-06 22:27:41 +0900

    今回の進行分

--- SamuraiEngineer_Action/Assets/Scripts/Player.cs
+++ SamuraiEngineer_Action/Assets/Scripts/Player.cs
@@ -6,10 +6,12 @@ public class Player : MonoBehaviour
 {
     public float speed = 5.0f;
     private Vector3 PlayerPos;
+    Animator PlayerAnimator;
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPos = GetComponent<Transform>().position;
+        //PlayerPos = GetComponent<Transform>().position;
+        PlayerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -19,27 +21,33 @@ public class Player : MonoBehaviour
         //Vector3 forwardDirection = transform.forward;
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            moveDirection.z = 5;
+            moveDirection.z = 1;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            moveDirection.z = -5;
+            moveDirection.z = -1;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            moveDirection.x = 5;
+            moveDirection.x = 1;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            moveDirection.x = -5;
+            moveDirection.x = -1;
         }
-        moveDirection.Normalize();
+        if()
+        moveDirection.Normalize();//³‹K‰»
 
-        Vector3 rotateDirecton = transform.position - PlayerPos;
+        //Vector3 rotateDirecton = transform.position - PlayerPos;
 
-        if (rotateDirecton.magnitude > 0.01f)
+        if (moveDirection.magnitude > 0.01f)
         {
-            transform.rotation = Quaternion.LookRotation(rotateDirecton);
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+            PlayerAnimator.SetBool("RUNNING", true);
+        }
+        else
+        {
+            PlayerAnimator.SetBool("RUNNING", false);
         }
 
         moveDirection *= speed * Time.deltaTime;
